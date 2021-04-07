3cbb1ea74b46a66499d1edffd6945a7d7ee0cea3
Mr.Khawaja <alveehawak360@gmail.com>
2021-04-07 15:08:22 +0600

    init

--- src/xyz/alveehawak/cdrop/MobDeath.java
+++ src/xyz/alveehawak/cdrop/MobDeath.java
@@ -1,13 +1,24 @@
 package xyz.alveehawak.cdrop;
 
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Witch;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class MobDeath implements Listener {
     @EventHandler
     public void onPlayerJoin(EntityDeathEvent event) {
-        Integer id = event.getEntity().getEntityId();
-        event.getEntity().getKiller().sendMessage(id.toString());
+        LivingEntity mob = event.getEntity();
+        if(mob instanceof Witch){
+            ItemStack stack = new ItemStack(Material.EMERALD, 10);
+            event.getDrops().add(stack);
+        }else if(mob instanceof Villager){
+            ItemStack stack = new ItemStack(Material.EMERALD, 5);
+            event.getDrops().add(stack);
+        }
     }
 }
